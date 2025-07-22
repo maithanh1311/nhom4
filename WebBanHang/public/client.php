9a7bb965513c66d02c2b02d78275cc15369cab4e
maithanh1311 <thanhzikb@gmail.com>
2025-07-22 21:40:46 +0700

    Delete WebBanHang directory

--- WebBanHang/public/client.php
+++
@@ -1,8 +0,0 @@
-<?php
-$controllerName = $_GET['controller'] ?? 'client';
-$action = $_GET['action'] ?? 'index';
-
-$controllerClass = $controllerName . 'Controller';
-require_once "./../controller/{$controllerClass}.php";
-$controller = new $controllerClass();
-$controller->$action();
